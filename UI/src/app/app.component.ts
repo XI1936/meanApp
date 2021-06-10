3acbb17582e4614fc5c17f9be03b806adebfd192
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-06-10 20:36:42 +0530

    more comp added

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,18 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'UI';
-  PostData;
-  constructor(private http : HttpClient){
-
-  }
   ngOnInit(){
   
   }
 
-  showPost(){
-    this.http.get('http://localhost:3000/post').subscribe((data)=>{
-      console.warn(data);
-      this.PostData=data;
-  });
-  }
+
 }
--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ShowButtonComponent } from './components/show-button/show-button.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ShowButtonComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
