1577b52eee3423d6359b20b4f48aebcc7d535c1e
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-06-14 22:00:08 +0530

    added canActived route

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationGuard } from './authentication.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path:"login" ,component: LoginComponent},
   { path: 'login', component: LoginComponent }, 
-  { path: 'home', component: HomeComponent }, 
-
+  { path: 'home', component: HomeComponent, canActivate:[AuthenticationGuard]}
 ];
 
 @NgModule({
