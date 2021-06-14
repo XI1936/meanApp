13fc7df6323ceab26a8ed228f339e3c3f8408d1a
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-06-14 16:00:12 +0530

    login page added

--- UI/src/app/components/login/login.component.ts
+++ UI/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppService } from 'src/app/service/app.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
+  form = new FormGroup({
+  uname: new FormControl('',[Validators.required]),
+  password:new FormControl('',[Validators.required])
+  });
 
   ngOnInit(): void {
   }
 
+  login(data){
+    console.warn(data);
+  }
+  clearAll(){
+    this.form.get('password').setValue("");
+    this.form.get('uname').setValue("");
+  }
 }
